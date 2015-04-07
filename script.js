56b252e7834a1dfcc0fcd6900a21ae4b9ecc5e61
Keshav Yamalapati <keshav@eggheadcreative.com>
2015-04-07 07:42:12 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// create the module and name it scotchApp
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+// configure our routes
+scotchApp.config(function ($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl: 'pages/about.html',
+            controller: 'aboutController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl: 'pages/contact.html',
+            controller: 'contactController'
+        });
+});
+
+// create the controller and inject Angular's $scope
+scotchApp.controller('mainController', function ($scope, $window, $http) {
+    // create a message to display in our view
+    $scope.searchThesaurus= function (item) {
+        $scope.greeting = item;
+        var url = "http://words.bighugelabs.com/api/2/a5660259832107069a17d6639b6b0a70/" + $scope.greeting + "/json";
+        $http.get(url).success(function (response) {
+            $scope.students = response;
+        });
+    };
+
+    $scope.getItems = function (item) {
+        //$window.alert(item);
+        var url = "http://words.bighugelabs.com/api/2/a5660259832107069a17d6639b6b0a70/" + item + "/json";
+        $http.get(url).success(function (response) {
+            $scope.students = response;
+        });
+    };
+});
+
+scotchApp.controller('aboutController', function ($scope, $window, $http) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+scotchApp.controller('contactController', function ($scope, $window, $http) {
+    $scope.message = 'Look! I am an about page.';
+});
